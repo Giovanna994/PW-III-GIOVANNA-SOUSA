c472ebe4bfe6117cda59864565ed5047a004feb4
Giovanna Sousa <161782726+Giovanna994@users.noreply.github.com>
2024-08-08 21:05:51 -0300

    Tentativa

--- ExCRUD/addprod.php
+++ ExCRUD/addprod.php
@@ -2,24 +2,35 @@
     include('conexao.php');
 
     $produto = $_POST['nomeProduto'];
-    $preco = $_POST['preco'];
+    $valor = $_POST['preco'];
     $descricao = $_POST['descricao'];
     
-    if (empty($produto) || empty($preco) || empty($descricao)) {
-        echo "Todos os campos são obrigatórios.";
-    } else {
-        // Prepare a declaração SQL
-        $stmt = $conn->prepare("INSERT INTO tbproduto VALUES(
-            null,
-            $produto,
-            $preco,
-            $descricao
-        )");
+    $stmt = $conn->prepare("INSERT INTO tbproduto VALUES(
+        null,
+        '$produto',
+        '$valor',
+        '$descricao'
+        )
+    ");
 
     $stmt->execute();
 
     $stmt = $conn->prepare("select * from tbproduto");
     $stmt->execute();
-    } 
 
     ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <button><a href="index.php">Voltar</a></button>
+    
+</body>
+</html>    
