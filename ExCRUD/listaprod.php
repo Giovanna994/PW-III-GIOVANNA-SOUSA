b25d88f878fc2faf2dbbe1fce2acce868faa454b
Giovanna Sousa <161782726+Giovanna994@users.noreply.github.com>
2024-08-08 21:56:40 -0300

    CRUD Quase completo

--- ExCRUD/listaprod.php
+++ ExCRUD/listaprod.php
@@ -4,24 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Lista de Produtos</title>
 </head>
 <body>
 
-<?php
+    <h1>Lista de Produtos</h1>
 
-?>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nome do Produto:</th>
+                <th scope="col">Preço:</th>
+                <th scope="col">Descrição do Produto:</th>
+                <button><a href="index.php">Voltar</a></button>
 
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Nome do Produto:</th>
-            <th scope="col">Preço:</th>
-            <th scope="col">Descrição do Produto:</th>
-            <button><a href="index.php">Voltar</a></button>
-
-        </tr>
+            </tr>
   </thead>
 
     <?php
