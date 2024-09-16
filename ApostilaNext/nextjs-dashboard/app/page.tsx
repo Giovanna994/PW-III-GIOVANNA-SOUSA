5a344842f5d227a1c2ede2c3b6223d6260707ceb
Giovanna Sousa <161782726+Giovanna994@users.noreply.github.com>
2024-09-16 19:54:51 -0300

    Feito até o capitulo 6

---
+++ ApostilaNext/nextjs-dashboard/app/page.tsx
@@ -0,0 +1,25 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
+import Image from 'next/image';
+ 
+// ...
+ 
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+        <AcmeLogo />
+        {/* ... */}
+        <Image
+        src="/hero-desktop.png"
+        width={1000}
+        height={760}
+        className="hidden md:block"
+        alt="Screenshots of the dashboard project showing desktop version"
+      />
+      </div>
+    </main>
+  );
+}
